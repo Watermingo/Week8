bbe4a8107e1b7b052e6c125fd902be7496a3f18c
Rayidla27 <washstate27@gmail.com>
2020-05-22 19:28:00 +1200

    Add files via upload

---
+++ Game3/Assets/Reasource/Scriptes/NPCFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFollow : MonoBehaviour
+{
+
+    public float speed;
+    private Transform target;
+
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+    }
+}
---
+++ Game3/Assets/Reasource/Scriptes/Talk.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Talk : MonoBehaviour
+{
+
+
+    public GameObject dialogueBox;
+    public Text DT;
+    public string Dialog;
+    public bool playerInRange;
+  
+    void Start()
+    {
+        
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if(dialogueBox.activeInHierarchy)
+            {
+                dialogueBox.SetActive(false);
+            }
+            else
+            {
+                dialogueBox.SetActive(true);
+                DT.text = Dialog;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+         {
+            playerInRange = true;
+         }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogueBox.SetActive(false);
+        }
+    }
+}
